3b69977dc7a22906e0eb0fae7a3ed3d1fbfd7af1
lahmedlabs <lahmedlabs@gmail.com>
2021-03-29 16:16:51 +0900

    init

--- refactoring-ExtractClass/main.cpp
+++ refactoring-ExtractClass/main.cpp
@@ -6,9 +6,10 @@ int main() {
 	Book refactoring = Book(
 		"Refactoring", "ISBN0011223344", "$44.99", "Martin Fowler", "fowler@acm.org");
 		cout << refactoring.toXml() << endl;
-
+		/* */
 	BookRF refactoringRf = BookRF(
 			"Refactoring", "ISBN0011223344", "$44.99", "Martin Fowler", "fowler@acm.org");
 		cout << refactoringRf.toXml() << endl;
 	return 0;
+
 }
